e1f58b06ae1fd586fc530865b210d1564fad34eb
arturplath <arturplath@users.noreply.github.com>
2020-07-20 11:28:47 +0200

    Added project schemes to work with Bitrise

---
+++ text-flipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  text-flipper
+//
+//  Created by Artur Plath on 20/07/2020.
+//  Copyright © 2020 Artur Plath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
