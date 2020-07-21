9cf0365a8aaadcc1e11ef06a42ae6b89097a8537
arturplath <arturplath@users.noreply.github.com>
2020-07-21 18:20:13 +0200

    First prototype, tested whether iPhone displays all characters properly

--- text-flipper/ViewController.swift
+++ text-flipper/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        var normal = Array(" abcdefghijklmnopqrstuvwxyz_,;.?!/\\'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+        var flipped  = Array(" ɐqɔpǝɟbɥıظʞןɯuodbɹsʇnʌʍxʎz‾'؛˙¿¡/\\,∀qϽᗡƎℲƃHIſʞ˥WNOԀὉᴚS⊥∩ΛMXʎZ0ƖᄅƐㄣϛ9ㄥ86")
 
+        var str = Array("Hello World!")
+        var strArray = Array(str)
+        var flippedString = ""
+        
+        for i in 0...str.count - 1 {
+            var letter = strArray[i]
+            var index = normal.firstIndex(of: letter)!
+                
+            flippedString += String(flipped[index])
+        }
+        
+        label.text = String(flippedString.reversed())
+  
+        }
+   
 }
-
